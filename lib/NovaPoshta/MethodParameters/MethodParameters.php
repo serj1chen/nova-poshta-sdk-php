af29b0f425e9349a714fd25352dd644472936a60
Pavel Petrik <pavel_petrik@mail.ru>
2017-02-07 00:25:31 +0200

    Добавил phpdoc

--- lib/NovaPoshta/MethodParameters/MethodParameters.php
+++ lib/NovaPoshta/MethodParameters/MethodParameters.php
@@ -8,6 +8,8 @@ use NovaPoshta\Core\BaseModel;
  * Использовать для передачи параметров в методы моделей
  *
  * Class MethodParameters
+ *
+ * @property array $DocumentRefs
  * @package NovaPoshta\MethodParameters
  */
 class MethodParameters extends BaseModel
