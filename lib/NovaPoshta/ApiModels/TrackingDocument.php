8a87c96e0256c9d5f2364f00328f5bae7b053d17
Vladyslav Tychyna <tychyna@alcora.eu>
2018-04-02 17:45:42 +0300

    Add correct method for tracking

---
+++ lib/NovaPoshta/ApiModels/TrackingDocument.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: vlad
+ * Date: 02.04.18
+ * Time: 15:54
+ */
+
+namespace NovaPoshta\ApiModels;
+
+
+class TrackingDocument
+{
+
+}
---
+++ lib/NovaPoshta/MethodParameters/TrackingDocument_getStatusDocuments.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Vladyslav Tychyna
+ * Date: 02.04.18
+ * Time: 16:12
+ */
+
+namespace NovaPoshta\MethodParameters;
+
+
+class TrackingDocument_getStatusDocuments
+{
+
+}
